2f44afc3ca59cbc4e91569db0668a02adec997c1
Adrian Cristea <adrian@softarea.ro>
2024-03-25 19:39:35 +0200

    Create my.sh

---
+++ my.sh
@@ -0,0 +1,59 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install --cask microsoft-edge
+brew install --cask adobe-acrobat-pro
+brew install --cask microsoft-word
+brew install --cask microsoft-excel
+brew install --cask microsoft-teams
+brew install --cask google-drive
+brew install --cask zoom
+brew install --cask lastpass
+brew install --cask fellow
+brew install --cask sublime-text
+brew install --cask slack
+brew install --cask evernote
+brew install --cask jetbrains-toolbox
+brew install --cask tableau
+brew install --cask tableau-prep
+brew install --cask http-toolkit
+brew install --cask lens
+brew install --cask mimestream
+brew install --cask mysqlworkbench
+brew install --cask navicat-for-mysql
+brew install --cask navicat-premium
+brew install --cask orbstack
+brew install --cask plex-media-server
+brew install --cask postman
+brew install --cask raycast
+brew install --cask setapp
+brew install --cask transmission
+brew install --cask viscosity
+brew install --cask warp
+brew install --cask wifi-explorer-pro
+brew install --cask whatsapp
+brew install --cask bitcoin-core
+brew install --cask gpg-suite
+
+brew install pyenv
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+brew install starship
+brew install mutt
+brew install kubectx
+brew install poetry
+brew install openssl
+brwe install sqlite
+brew install zlib
+brew install libpng
+brew install xz
+brew install readline
+brew install mysql-client
+brew install percona-server
+brew install postgresql
+brew install git-crypt
+brew install csvq
+brwe install black
+brew install ripgrep
+brew install socat
