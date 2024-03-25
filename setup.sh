9fc0e7f687a6748c7a5b91deec6fc97ac2a100d6
Adrian Cristea <adrian@softarea.ro>
2024-03-25 17:36:53 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install --cask google-chrome
+brew install --cask adobe-acrobat-reader
+brew install --cask microsoft-word
+brew install --cask microsoft-excel
+brew install --cask microsoft-powerpoint
+brew install --cask google-drive
+brew install --cask zoom
+brew install --cask notion
+brew install --cask lastpass
+brew install --cask deepl
+brew install --cask fellow
+brew install --cask miro
+brew install --cask sublime-text
+brew install --cask berrycast
+brew install --cask slack
